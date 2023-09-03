d0f5a523c0b5a85bef6b64ff384a74806042b263
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-09-03 18:16:34 -0300

    refactor: folder separation

---
+++ CFKA.Health.Mobile/Pages/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CFKA.Health.Mobile.ViewModel;
+
+namespace CFKA.Health.Mobile;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage(LoginViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
