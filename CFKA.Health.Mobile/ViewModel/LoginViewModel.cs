bda4f56ebee41a1a6aa9fa77b7a230435dcf9a5a
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-09-03 18:16:45 -0300

    feat: viewModel for data binding

---
+++ CFKA.Health.Mobile/ViewModel/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace CFKA.Health.Mobile.ViewModel;
+
+public partial class LoginViewModel : ObservableObject
+{
+    [ICommand]
+    async Task MoveBack()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+}
---
+++ CFKA.Health.Mobile/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace CFKA.Health.Mobile.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+
+    [ICommand]
+    async Task MoveToLogin()
+    {
+        await Shell.Current.GoToAsync(nameof(LoginPage));
+    }
+}
